3e189281ef0027a5626b4ca9a47d2457c06cf996
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-09-12 21:40:59 +0000

    Impemented home.dart

--- food_demo/lib/home_page.dart
+++ food_demo/lib/home_page.dart
@@ -5,6 +5,84 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dessert Recipes'),
+      ),
+      body: FoodCard(),
+      // body: ListView.builder(
+      //   itemBuilder: (context, index) => const FoodCard(),
+      // ),
+    );
+  }
+}
+
+class FoodCard extends StatelessWidget {
+  const FoodCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Container(
+            height: 190,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.yellow,
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0, 9),
+                  blurRadius: 8,
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width - 170,
+                    child: Text(
+                      'Lemon Cheesecake',
+                      style: theme.textTheme.titleLarge,
+                      maxLines: 2,
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width - 170,
+                    child: Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                      style: theme.textTheme.bodyMedium,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: -2,
+          bottom: -15,
+          child: SizedBox(
+            height: 150,
+            width: 150,
+            child: Image.asset(
+              'assets/images/04-fluffy-cake.png',
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
