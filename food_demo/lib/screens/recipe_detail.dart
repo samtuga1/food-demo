598f714e10e94b47511718aae07e3802a51b58b2
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-09-14 20:03:32 +0000

    update

--- food_demo/lib/screens/recipe_detail.dart
+++ food_demo/lib/screens/recipe_detail.dart
@@ -1,7 +1,8 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'dart:math' show pi;
 import '../utilities/ingredient_item.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class RecipeDetail extends StatefulWidget {
   static const routeName = '/detail';
@@ -13,133 +14,170 @@ class RecipeDetail extends StatefulWidget {
 
 class _RecipeDetailState extends State<RecipeDetail> {
   ScrollController scrollController = ScrollController();
-  ValueNotifier<ScrollDirection> scrollDirection =
-      ValueNotifier<ScrollDirection>(ScrollDirection.forward);
+  double angle = 2.0;
+
+  void rotateRecipe() {
+    setState(() {
+      if (scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        angle += scrollController.offset / 1000;
+      }
+      if (scrollController.position.userScrollDirection ==
+          ScrollDirection.forward) {
+        angle -= scrollController.offset / 1000;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(rotateRecipe);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    scrollController.removeListener(rotateRecipe);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final image = ModalRoute.of(context)?.settings.arguments as String;
     final theme = Theme.of(context);
     return Scaffold(
-      body: NotificationListener<UserScrollNotification>(
-        onNotification: (notification) {
-          if (notification.direction == ScrollDirection.forward) {
-            scrollDirection.value = ScrollDirection.forward;
-          }
-          if (notification.direction == ScrollDirection.reverse) {
-            scrollDirection.value = ScrollDirection.reverse;
-          }
-          return true;
-        },
-        child: CustomScrollView(
-          controller: scrollController,
-          slivers: [
-            SliverAppBar(
-              collapsedHeight: 200,
-              pinned: true,
-              leadingWidth: 46,
-              automaticallyImplyLeading: false,
-              leading: Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: const CircleAvatar(
-                    backgroundColor: Colors.black54,
-                    child: Icon(Icons.arrow_back),
-                  ),
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        controller: scrollController,
+        slivers: [
+          SliverAppBar(
+            stretch: true,
+            collapsedHeight: 200,
+            pinned: true,
+            leadingWidth: 46,
+            automaticallyImplyLeading: false,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: const CircleAvatar(
+                  backgroundColor: Colors.black54,
+                  child: Icon(Icons.arrow_back),
                 ),
               ),
-              expandedHeight: 340,
-              flexibleSpace: Stack(
-                children: [
-                  Container(
-                    height: 340,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/bg-food.png'),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(25),
-                        bottomRight: Radius.circular(25),
-                      ),
-                      color: Colors.yellow,
+            ),
+            expandedHeight: 320,
+            flexibleSpace: Stack(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(25),
+                      bottomRight: Radius.circular(25),
+                    ),
+                    color: Colors.yellow,
+                  ),
+                  child: FlexibleSpaceBar(
+                    stretchModes: const [StretchMode.zoomBackground],
+                    background: Image.asset(
+                      'assets/images/bg-food.png',
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 20.0),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: ValueListenableBuilder<ScrollDirection>(
-                          valueListenable: scrollDirection,
-                          builder: (context, scrollDirection, child) {
-                            int angle = 2;
-                            while (scrollDirection == ScrollDirection.forward) {
-                              angle += 1;
-                              print(angle);
-                            }
-                            return Transform.rotate(
-                              angle: pi * 2,
-                              child: SizedBox(
-                                height: 230,
-                                width: 230,
-                                child: Image.asset(
-                                  'assets/images/15-apple-pie.png',
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Transform.rotate(
+                      angle: angle * pi,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Hero(
+                          tag: image,
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  offset: Offset.zero,
+                                  blurRadius: 7.5,
                                 ),
-                              ),
-                            );
-                          }),
+                              ],
+                            ),
+                            height: 230,
+                            width: 230,
+                            child: Image.asset(
+                              image,
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Lemon Cheesecake',
-                          style: theme.textTheme.titleLarge,
-                          maxLines: 2,
-                          textAlign: TextAlign.left,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
-                          style: theme.textTheme.bodyMedium,
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'Ingredients',
-                          style: theme.textTheme.titleLarge,
-                          maxLines: 2,
-                          textAlign: TextAlign.left,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        IngredientItem(theme: theme),
-                        IngredientItem(theme: theme),
-                        IngredientItem(theme: theme),
-                        IngredientItem(theme: theme),
-                        IngredientItem(theme: theme),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Lemon Cheesecake',
+                        style: theme.textTheme.titleLarge,
+                        maxLines: 2,
+                        textAlign: TextAlign.left,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                        style: theme.textTheme.bodyMedium,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Ingredients',
+                        style: theme.textTheme.titleLarge,
+                        maxLines: 2,
+                        textAlign: TextAlign.left,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ...List.generate(
+                        10,
+                        (i) => IngredientItem(theme: theme)
+                            .animate()
+                            .fadeIn(
+                                //delay: Duration(milliseconds: 50 * i),
+                                duration: Duration(milliseconds: 100 * i))
+                            .slide(
+                              delay: Duration(milliseconds: 100 * i),
+                              duration: const Duration(milliseconds: 400),
+                              begin: const Offset(0, 0.5),
+                              end: Offset.zero,
+                            ),
+                      ).toList(),
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
