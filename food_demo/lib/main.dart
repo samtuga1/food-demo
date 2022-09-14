c3f44bfcb6505f316a29bf000558ce270f48cc3e
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-09-14 16:15:38 +0000

    update

--- food_demo/lib/main.dart
+++ food_demo/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:food_demo/home_page.dart';
+import 'package:food_demo/utilities/recipe_model.dart';
+import 'package:food_demo/utilities/recipes.dart';
 import 'package:food_demo/utilities/theme_data.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +15,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: MyTheme.app_theme(),
-      title: 'Flutter Demo',
-      home: const HomePage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => RecipeModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Recipes(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: MyTheme.app_theme,
+        title: 'Flutter Demo',
+        home: const HomePage(),
+      ),
     );
   }
 }
